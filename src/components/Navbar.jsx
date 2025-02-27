226283b7c4dc57d0ac283096d99f179b713ada75
Asir Khan <ashir16592@gmail.com>
2025-02-27 13:30:30 +0530

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 import { useAuth } from '../store/auth'
 import { ToastContainer } from 'react-toastify'
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   const {isLoggedIn, LogoutUser} = useAuth()
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <ToastContainer
@@ -22,21 +27,22 @@ const Navbar = () => {
       />
     <div className='container'>
       <div className='div1'>AsirKhan</div>
-      <div>
-        <nav>
+        <div className='menu-icon' onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+        <nav className={isOpen ? 'nav-menu active' : 'nav-menu'}>
           <ul>
-            <li><NavLink to='/'>Home</NavLink></li>
-            <li><NavLink to='/about'>About</NavLink></li>
-            <li><NavLink to='/contact'>Contact</NavLink></li>
-            <a><li><NavLink to='/services'>Services</NavLink></li></a>
-            {isLoggedIn ? ( <li><NavLink to='/logout' onClick={LogoutUser}>Log Out</NavLink></li> 
-            ) : (<> <li><NavLink to='/login'>Login</NavLink></li>
-            <li><NavLink to='/signup'>SignUp</NavLink></li>
+            <li><NavLink to='/' onClick={toggleMenu}>Home</NavLink></li>
+            <li><NavLink to='/about' onClick={toggleMenu}>About</NavLink></li>
+            <li><NavLink to='/contact' onClick={toggleMenu}>Contact</NavLink></li>
+            <a><li><NavLink to='/services' onClick={toggleMenu}>Services</NavLink></li></a>
+            {isLoggedIn ? ( <li><NavLink to='/logout' onClick={() => {LogoutUser(); toggleMenu();}}>Log Out</NavLink></li> 
+            ) : (<> <li><NavLink to='/login' onClick={toggleMenu}>Login</NavLink></li>
+            <li><NavLink to='/signup' onClick={toggleMenu}>SignUp</NavLink></li>
             </>)}
-              <li><NavLink to='/admin'>Admin</NavLink></li>
+              <li><NavLink to='/admin' onClick={toggleMenu}>Admin</NavLink></li>
             </ul>
-        </nav>
-      </div>    
+        </nav>      
     </div>
     </>
   )
