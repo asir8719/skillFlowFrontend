603a7b00683e4bfcabc80b617f849dd8ba64cdb3
Asir Khan <ashir16592@gmail.com>
2025-04-06 17:40:05 +0530

    init2

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  optimizeDeps: {
-    include: ['gsap', 'gsap/ScrollTrigger'],
-    force: true,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          gsap: ['gsap'], // Ensure GSAP is bundled correctly
-        },
-      },
-    },
-  },
 })
